feb65436bc715bded5ff253fb1c7505e58a10fc2
SveerPal <yashveersoftwere06@gmail.com>
2024-06-21 01:30:47 +0530

    format

--- yash/laravel/ecommerce-app/resources/views/admin/app.blade.php
+++ yash/laravel/ecommerce-app/resources/views/admin/app.blade.php
@@ -1,90 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>@yield('title') - {{ config('app.name') }}</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
-</head>
-<body class="app sidebar-mini">
-    @include('admin.partials.header')
-    @include('admin.partials.sidebar')
-    <main class="app-content">
-        @yield('content')
-    </main>
-    <script src="{{ asset('backend/js/jquery-3.7.0.min.js') }}"></script>
-    <script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('backend/js/main.js') }}"></script>
-    <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script>
 
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts@5.4.3/dist/echarts.min.js"></script>
-    <script type="text/javascript">
-      const salesData = {
-      	xAxis: {
-      		type: 'category',
-      		data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      	},
-      	yAxis: {
-      		type: 'value',
-      		axisLabel: {
-      			formatter: '${value}'
-      		}
-      	},
-      	series: [
-      		{
-      			data: [150, 230, 224, 218, 135, 147, 260],
-      			type: 'line',
-      			smooth: true
-      		}
-      	],
-      	tooltip: {
-      		trigger: 'axis',
-      		formatter: "<b>{b0}:</b> ${c0}"
-      	}
-      }
-      
-      const supportRequests = {
-      	tooltip: {
-      		trigger: 'item'
-      	},
-      	legend: {
-      		orient: 'vertical',
-      		left: 'left'
-      	},
-      	series: [
-      		{
-      			name: 'Support Requests',
-      			type: 'pie',
-      			radius: '50%',
-      			data: [
-      				{ value: 300, name: 'In Progress' },
-      				{ value: 50, name: 'Delayed' },
-      				{ value: 100, name: 'Complete' }
-      			],
-      			emphasis: {
-      				itemStyle: {
-      					shadowBlur: 10,
-      					shadowOffsetX: 0,
-      					shadowColor: 'rgba(0, 0, 0, 0.5)'
-      				}
-      			}
-      		}
-      	]
-      };
-      
-      const salesChartElement = document.getElementById('salesChart');
-      const salesChart = echarts.init(salesChartElement, null, { renderer: 'svg' });
-      salesChart.setOption(salesData);
-      new ResizeObserver(() => salesChart.resize()).observe(salesChartElement);
-      
-      const supportChartElement = document.getElementById("supportRequestChart")
-      const supportChart = echarts.init(supportChartElement, null, { renderer: 'svg' });
-      supportChart.setOption(supportRequests);
-      new ResizeObserver(() => supportChart.resize()).observe(supportChartElement);
-    </script>
-    
+<head>
+	<title>@yield('title') - {{ config('app.name') }}</title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
+	<link rel="stylesheet" type="text/css"
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<link rel="stylesheet" type="text/css"
+		href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}" />
+</head>
+
+<body class="app sidebar-mini">
+	@include('admin.partials.header')
+	@include('admin.partials.sidebar')
+	<main class="app-content">
+		@yield('content')
+	</main>
+	<script src="{{ asset('backend/js/jquery-3.7.0.min.js') }}"></script>
+	<script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('backend/js/main.js') }}"></script>
+	<script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script>
+
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts@5.4.3/dist/echarts.min.js"></script>
+	<script type="text/javascript">
+		const salesData = {
+			xAxis: {
+				type: 'category',
+				data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+			},
+			yAxis: {
+				type: 'value',
+				axisLabel: {
+					formatter: '${value}'
+				}
+			},
+			series: [
+				{
+					data: [150, 230, 224, 218, 135, 147, 260],
+					type: 'line',
+					smooth: true
+				}
+			],
+			tooltip: {
+				trigger: 'axis',
+				formatter: "<b>{b0}:</b> ${c0}"
+			}
+		}
+
+		const supportRequests = {
+			tooltip: {
+				trigger: 'item'
+			},
+			legend: {
+				orient: 'vertical',
+				left: 'left'
+			},
+			series: [
+				{
+					name: 'Support Requests',
+					type: 'pie',
+					radius: '50%',
+					data: [
+						{ value: 300, name: 'In Progress' },
+						{ value: 50, name: 'Delayed' },
+						{ value: 100, name: 'Complete' }
+					],
+					emphasis: {
+						itemStyle: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
+		};
+
+		const salesChartElement = document.getElementById('salesChart');
+		const salesChart = echarts.init(salesChartElement, null, { renderer: 'svg' });
+		salesChart.setOption(salesData);
+		new ResizeObserver(() => salesChart.resize()).observe(salesChartElement);
+
+		const supportChartElement = document.getElementById("supportRequestChart")
+		const supportChart = echarts.init(supportChartElement, null, { renderer: 'svg' });
+		supportChart.setOption(supportRequests);
+		new ResizeObserver(() => supportChart.resize()).observe(supportChartElement);
+	</script>
+
 </body>
+
 </html>
--- yash/laravel/ecommerce-app/resources/views/admin/partials/sidebar.blade.php
+++ yash/laravel/ecommerce-app/resources/views/admin/partials/sidebar.blade.php
@@ -1,13 +1,14 @@
 <!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-    <aside class="app-sidebar">
-      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Image">
+<aside class="app-sidebar">
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar"
+            src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Image">
         <div>
-          <p class="app-sidebar__user-name">John Doe</p>
-          <p class="app-sidebar__user-designation">Frontend Developer</p>
+            <p class="app-sidebar__user-name">John Doe</p>
+            <p class="app-sidebar__user-designation">Frontend Developer</p>
         </div>
-      </div>
-      <ul class="app-menu">
+    </div>
+    <ul class="app-menu">
         <li>
             <a class="app-menu__item active" href="dashboard.html">
                 <i class="app-menu__icon bi bi-speedometer"></i>
@@ -24,20 +25,21 @@
                     <a class="treeview-item" href="#"><i class="icon bi bi-circle"></i> Admin Users</a>
                 </li>
                 <li>
-                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreferrer"><i class="icon bi bi-circle"></i> Roles</a>
+                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreferrer"><i
+                            class="icon bi bi-circle"></i> Roles</a>
                 </li>
                 <li>
                     <a class="treeview-item" href="#"><i class="icon bi bi-circle"></i> Permissions</a>
                 </li>
             </ul>
         </li>
-       
-        
+
+
         <li>
             <a class="app-menu__item" href="#"><i class="app-menu__icon bi bi-gear"></i>
                 <span class="app-menu__label">Settings</span>
             </a>
         </li>
-        
+
     </ul>
-    </aside>
+</aside>
